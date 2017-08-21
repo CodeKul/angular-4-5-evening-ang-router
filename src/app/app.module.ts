ab9672c736fea2b09dd194d9e9affe84c42bb82c
aniruddha <aniruddha.kudalkar@gmail.com>
2017-08-21 17:17:41 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
+import { MyRoutingModule } from './app.router';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashComponent } from './dash/dash.component';
+import { ForgetPassComponent } from './forget-pass/forget-pass.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashComponent,
+    ForgetPassComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MyRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
